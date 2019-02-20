9b8c8784539784183021d00e083fa0fa16f06e00
Grayson Beam <33629589+Pyreandice@users.noreply.github.com>
2019-02-19 21:52:00 -0600

    Add files via upload

--- Color-Thermostat-GUI-GraysonBeam/Color-Thermostat-GUI-GraysonBeam/Form1.Designer.cs
+++ Color-Thermostat-GUI-GraysonBeam/Color-Thermostat-GUI-GraysonBeam/Form1.Designer.cs
@@ -39,51 +39,58 @@
             // 
             // TemperatureTextBox
             // 
-            this.TemperatureTextBox.Location = new System.Drawing.Point(228, 6);
+            this.TemperatureTextBox.Location = new System.Drawing.Point(304, 7);
+            this.TemperatureTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TemperatureTextBox.Name = "TemperatureTextBox";
-            this.TemperatureTextBox.Size = new System.Drawing.Size(39, 20);
+            this.TemperatureTextBox.Size = new System.Drawing.Size(51, 22);
             this.TemperatureTextBox.TabIndex = 0;
             // 
             // TemperatureLabel
             // 
             this.TemperatureLabel.AutoSize = true;
             this.TemperatureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.TemperatureLabel.Location = new System.Drawing.Point(12, 9);
+            this.TemperatureLabel.Location = new System.Drawing.Point(16, 11);
+            this.TemperatureLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TemperatureLabel.Name = "TemperatureLabel";
-            this.TemperatureLabel.Size = new System.Drawing.Size(210, 17);
+            this.TemperatureLabel.Size = new System.Drawing.Size(244, 20);
             this.TemperatureLabel.TabIndex = 1;
             this.TemperatureLabel.Text = "Enter a temperture value. 1-100";
             // 
             // TemperatureResultRichTextBox
             // 
-            this.TemperatureResultRichTextBox.Location = new System.Drawing.Point(44, 79);
+            this.TemperatureResultRichTextBox.Location = new System.Drawing.Point(59, 97);
+            this.TemperatureResultRichTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TemperatureResultRichTextBox.Name = "TemperatureResultRichTextBox";
-            this.TemperatureResultRichTextBox.Size = new System.Drawing.Size(252, 186);
+            this.TemperatureResultRichTextBox.Size = new System.Drawing.Size(335, 228);
             this.TemperatureResultRichTextBox.TabIndex = 2;
             this.TemperatureResultRichTextBox.Text = "";
             // 
             // TemperatureNumericUpDown
             // 
-            this.TemperatureNumericUpDown.Location = new System.Drawing.Point(207, 34);
+            this.TemperatureNumericUpDown.Location = new System.Drawing.Point(276, 42);
+            this.TemperatureNumericUpDown.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TemperatureNumericUpDown.Name = "TemperatureNumericUpDown";
-            this.TemperatureNumericUpDown.Size = new System.Drawing.Size(39, 20);
+            this.TemperatureNumericUpDown.Size = new System.Drawing.Size(52, 22);
             this.TemperatureNumericUpDown.TabIndex = 3;
+            this.TemperatureNumericUpDown.ValueChanged += new System.EventHandler(this.TemperatureNumericUpDown_ValueChanged);
             // 
             // NumericUpDownLabel
             // 
             this.NumericUpDownLabel.AutoSize = true;
             this.NumericUpDownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.NumericUpDownLabel.Location = new System.Drawing.Point(98, 34);
+            this.NumericUpDownLabel.Location = new System.Drawing.Point(131, 42);
+            this.NumericUpDownLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NumericUpDownLabel.Name = "NumericUpDownLabel";
-            this.NumericUpDownLabel.Size = new System.Drawing.Size(103, 17);
+            this.NumericUpDownLabel.Size = new System.Drawing.Size(120, 20);
             this.NumericUpDownLabel.TabIndex = 4;
             this.NumericUpDownLabel.Text = "+ or - 1 degree\r\n";
             // 
             // TemperatureButton
             // 
-            this.TemperatureButton.Location = new System.Drawing.Point(273, 4);
+            this.TemperatureButton.Location = new System.Drawing.Point(364, 5);
+            this.TemperatureButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TemperatureButton.Name = "TemperatureButton";
-            this.TemperatureButton.Size = new System.Drawing.Size(52, 23);
+            this.TemperatureButton.Size = new System.Drawing.Size(69, 28);
             this.TemperatureButton.TabIndex = 5;
             this.TemperatureButton.Text = "Go";
             this.TemperatureButton.UseVisualStyleBackColor = true;
@@ -91,15 +98,16 @@
             // 
             // ColorsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 295);
+            this.ClientSize = new System.Drawing.Size(459, 363);
             this.Controls.Add(this.TemperatureButton);
             this.Controls.Add(this.NumericUpDownLabel);
             this.Controls.Add(this.TemperatureNumericUpDown);
             this.Controls.Add(this.TemperatureResultRichTextBox);
             this.Controls.Add(this.TemperatureLabel);
             this.Controls.Add(this.TemperatureTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ColorsForm";
             this.Text = "Thermostat Temperture Calculator";
             ((System.ComponentModel.ISupportInitialize)(this.TemperatureNumericUpDown)).EndInit();
--- Color-Thermostat-GUI-GraysonBeam/Color-Thermostat-GUI-GraysonBeam/Form1.cs
+++ Color-Thermostat-GUI-GraysonBeam/Color-Thermostat-GUI-GraysonBeam/Form1.cs
@@ -12,28 +12,86 @@ using System.Windows.Forms;
 
 namespace Color_Thermostat_GUI_GraysonBeam
 {
+    using System.Diagnostics.CodeAnalysis;
+
     public partial class ColorsForm : Form
     {
-        private const int NumZero = 0;
-        private const int NumOne = 1;
-        private const int NumTwentyFive = 25;
-        private const int NumTwentySix = 26;
-        private const int NumFifty = 50;
-        private const int NumFiftyOne = 51;
-        private const int NumSeventyFive = 75;
-        private const int NumSeventySix = 76;
-        private const int NumOneHundred = 100;
-        private const int NumTwoFiftyFive = 255;
+        /// <summary>
+        /// The num zero.
+        /// </summary>
+        private const int Zero = 0;
+
+        /// <summary>
+        /// The num one.
+        /// </summary>
+        private const int One = 1;
+
+        /// <summary>
+        /// The num twenty five.
+        /// </summary>
+        private const int TwentyFive = 25;
+
+        /// <summary>
+        /// The num twenty six.
+        /// </summary>
+        private const int TwentySix = 26;
+
+        /// <summary>
+        /// The num fifty.
+        /// </summary>
+        private const int Fifty = 50;
+
+        /// <summary>
+        /// The num fifty one.
+        /// </summary>
+        private const int FiftyOne = 51;
+
+        /// <summary>
+        /// The num seventy five.
+        /// </summary>
+        private const int SeventyFive = 75;
+
+        /// <summary>
+        /// The num seventy six.
+        /// </summary>
+        private const int SeventySix = 76;
+
+        /// <summary>
+        /// The num one hundred.
+        /// </summary>
+        private const int OneHundred = 100;
+
+        /// <summary>
+        /// The num two fifty five.
+        /// </summary>
+        private const int TwoFiftyFive = 255;
+
+        /// <summary>
+        ///  The num two fifty six.
+        /// </summary>
+        private const int TwoFiftySix = 256;
+
+        /// <summary>
+        /// The conversion factor.
+        /// </summary>
+        private const int ConversionFactor = 65536;
 
         public ColorsForm()
         {
             this.InitializeComponent();
         }
 
-        public void CalculateTemperatureColor()
+        /// <summary>
+        /// The calculate temperature color.
+        /// </summary>
+        /// <param name="userColor">
+        /// The user color.
+        /// </param>
+        [SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1126:PrefixCallsCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+        private void CalculateTemperatureColor()
         {
             int.TryParse(this.TemperatureTextBox.Text, out var temperature);
-            if (temperature < NumOne || temperature > NumOneHundred)
+            if (temperature < One || temperature > OneHundred)
             {
                 this.TemperatureTextBox.BackColor = Color.IndianRed;
             }
@@ -42,49 +100,69 @@ namespace Color_Thermostat_GUI_GraysonBeam
                 this.TemperatureTextBox.BackColor = Color.White;
             }
 
-            if (temperature >= NumZero && temperature <= NumTwentyFive)
+            if (temperature >= Zero && temperature <= TwentyFive)
             {
-                var zeta = (temperature - NumOne) / (NumTwentyFive - NumOne);
-                var tote = NumTwoFiftyFive * zeta + NumTwoFiftyFive * (NumOne - zeta);
-            }
-            else if (temperature >= NumTwentySix && temperature <= NumFifty)
-            {
-                var zeta = (temperature - NumTwentySix) / (NumFifty - NumTwentySix);
-                var tote = NumTwoFiftyFive * zeta + NumTwoFiftyFive * (NumOne - zeta);
+                var redValue = (double)Zero;
+                var greenValue = ((double)temperature - (double)One) / ((double)TwentyFive - (double)One);
+                var blueValue = (TwoFiftyFive * greenValue) + (TwoFiftyFive * (One - greenValue));
+                var userColor = (ConversionFactor * redValue) + (greenValue * TwoFiftySix) + blueValue;
 
-
+                this.TemperatureResultRichTextBox.Text = string.Format(
+                    @"The user has entered {0:G} {1} The RGB values are ({2:G3}, {3:G3}, {4:G3}) {5} The color value would be {6:G3}",
+                    temperature,
+                    Environment.NewLine,
+                    redValue,
+                    greenValue,
+                    blueValue,
+                    Environment.NewLine,
+                    userColor);
+                this.BackColor = Color.FromArgb((int)redValue, (int)greenValue, (int)blueValue);
             }
-            else if (temperature >= NumFiftyOne && temperature <= NumSeventyFive)
+            else if (temperature >= TwentySix && temperature <= Fifty)
             {
-                // do this
+                var redValue = (temperature - TwentySix) / (Fifty - TwentySix);
+                var greenValue = (TwoFiftyFive * redValue) + (TwoFiftyFive * (One - redValue));
+                var blueValue = 0; // temp till formula is made
             }
-            else if (temperature >= NumSeventySix && temperature <= NumOneHundred)
+            else if (temperature >= FiftyOne && temperature <= SeventyFive)
             {
-                // do this
+                var redValue = (temperature - FiftyOne) / SeventyFive * (SeventyFive - FiftyOne);
+                var greenValue = (TwoFiftyFive * redValue) + (TwoFiftyFive * (One - redValue));
+                var blueValue = 0; // temp till formula is made
+            }
+            else if (temperature >= SeventySix && temperature <= OneHundred)
+            {
+                var redValue = (temperature - SeventySix) / OneHundred * (OneHundred - SeventySix);
+                var greenValue = (TwoFiftyFive * redValue) + (TwoFiftyFive * (One - redValue));
+                var blueValue = 0; // temp till formula is made
             }
         }
 
-        public int ParseTemperatureTextBox(out int temperatureValue)
-        {
-            int.TryParse(this.TemperatureTextBox.Text, out var temperature);
-            if (temperature < NumOne || temperature > NumOneHundred)
-            {
-                this.TemperatureTextBox.BackColor = Color.IndianRed;
-            }
-            else
-            {
-                this.TemperatureTextBox.BackColor = Color.White;
-                this.CalculateTemperatureColor();
-            }
-            temperatureValue = temperature;
+        //public int ParseTemperatureTextBox(out int temperatureValue)
+        //{
+        //    int.TryParse(this.TemperatureTextBox.Text, out var temperature);
+        //    if (temperature < One || temperature > OneHundred)
+        //    {
+        //        this.TemperatureTextBox.BackColor = Color.IndianRed;
+        //    }
+        //    else
+        //    {
+        //        this.TemperatureTextBox.BackColor = Color.White;
+        //        this.CalculateTemperatureColor(out double userColor);
+        //    }
+        //    temperatureValue = temperature;
 
-            return temperatureValue;
-        }
+        //    return temperatureValue;
+        //}
 
         private void TemperatureButton_Click(object sender, EventArgs e)
         {
-            ParseTemperatureTextBox(out int temperatureValue);
-            this.TemperatureResultRichTextBox.Text = $@"{temperatureValue:G}";
+            this.CalculateTemperatureColor();
+        }
+
+        private void TemperatureNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            this.CalculateTemperatureColor();
         }
     }
 }
